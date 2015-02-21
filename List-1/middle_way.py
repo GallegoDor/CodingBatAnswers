68fd4487ec2c3661e1e9c10f57ffe81e543e4f97
Dor Gallego <dor.gallego@gmail.com>
2015-02-21 14:51:52 -0500

    Update and rename List-1 to List-1/middle_way.py

---
+++ List-1/middle_way.py
@@ -0,0 +1,8 @@
+def middle_way(a, b):
+  mid_one = a[len(a) / 2]
+  mid_two = b[len(b) / 2]
+  return [mid_one, mid_two] 
+  
+#Alternitavely couldve done one-liner return [a[1], b[1]]
+#But I wanted a more general use functions, that could work for lengths > 3 which are odd.
+#If, there is no one middle index, there are two.
