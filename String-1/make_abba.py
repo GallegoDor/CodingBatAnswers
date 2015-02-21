cdd3502a8ed6c978ff63aa07c4e119d3c6e961e5
Dor Gallego <dor.gallego@gmail.com>
2015-02-21 01:04:29 -0500

    Update and rename String-1 to String-1/make_abba.py

---
+++ String-1/make_abba.py
@@ -0,0 +1,7 @@
+#Instructions
+#Given two strings, a and b, return the result of putting them together in the order abba, 
+#e.g. "Hi" and "Bye" returns "HiByeByeHi". 
+
+
+def make_abba(a, b):
+  return "%s%s%s%s" % (a,b,b,a)
